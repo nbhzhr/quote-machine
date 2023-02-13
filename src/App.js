ce06852bade06d4082435e70c6a2341011a96573
Nabilah Azhar <nabilahazhar98@gmail.com>
2023-02-13 14:15:55 +0800

    Initial commit with basic quote generation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const APIURL = "https://dummyjson.com/quotes/random";
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: 'Click for a new quote',
+      author: '...'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = () => {
+    fetch(APIURL)
+    .then((data) => data.json())
+    .then((item) => {
+      this.setState({
+        quote: item.quote,
+        author: item.author
+      })
+    });
+  }
+
+  render(){
+    return(
+      <div class="wrapper">
+        <div class="container" id="quote-box">
+          <h3 id="text">{this.state.quote}</h3>
+          <h4 id="author">{this.state.author}</h4>
+          <button id="new-quote" onClick={this.handleClick}>Click for more</button>
+          <a href="twitter.com/intent/tweet" id="tweet-quote">Tweet</a>
+        </div>
+      </div>
+    )
+  }
+  
 }
 
 export default App;
